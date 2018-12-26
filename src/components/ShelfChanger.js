23a28094de49b24fcc6f9faac84028bb94159871
Laurens Deprost <ldeprost@gmail.com>
2018-12-26 13:18:31 +0100

    feat: Add ShelfChanger component

---
+++ src/components/ShelfChanger.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { SHELF_TYPE } from "../Constants";
+
+class ShelfChanger extends Component {
+  static propTypes = {
+    book: PropTypes.object.isRequired,
+    books: PropTypes.array.isRequired,
+    updateShelf: PropTypes.func.isRequired
+  };
+
+  updateShelf = event => {
+    this.props.book.shelf !== event.target.value &&
+      this.props.updateShelf(this.props.book, event.target.value);
+  };
+
+  render() {
+    const { book, books } = this.props;
+
+    // make sure book displays correct shelf
+    let currentShelf = SHELF_TYPE.none.id;
+    for (let shelfBook of books) {
+      if (shelfBook.id === book.id) {
+        currentShelf = shelfBook.shelf;
+        break;
+      }
+    }
+
+    return (
+      <div className="book-shelf-changer">
+        <select onChange={this.updateShelf} defaultValue={currentShelf}>
+          <option value="none" disabled>
+            Move to...
+          </option>
+          {Object.keys(SHELF_TYPE).map(type => (
+            <option key={type} value={type}>
+              {SHELF_TYPE[type].text}
+            </option>
+          ))}
+        </select>
+      </div>
+    );
+  }
+}
+
+export default ShelfChanger;
