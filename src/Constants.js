817662421134cfd981d34eb1bb43745f236efc7c
Laurens Deprost <ldeprost@gmail.com>
2018-12-26 11:59:38 +0100

    refactor: Add constants file

---
+++ src/Constants.js
@@ -0,0 +1,18 @@
+export const SHELF_TYPE = {
+  currentlyReading: {
+    id: "currentlyReading",
+    text: "Currently Reading"
+  },
+  wantToRead: {
+    id: "wantToRead",
+    text: "Want to Read"
+  },
+  read: {
+    id: "read",
+    text: "Read"
+  },
+  none: {
+    id: "none",
+    text: "None"
+  }
+};
